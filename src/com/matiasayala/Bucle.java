98eea03bc111a5961a92d43e63a9ed22a5df0727
M-A-web <matias_ayala87@outlook.com>
2023-01-17 13:06:45 -0300

    cargando proyecto

---
+++ src/com/matiasayala/Bucle.java
@@ -0,0 +1,13 @@
+package com.matiasayala;
+
+public class Bucle {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"matias", "fede", "manolo", "lola", "maria"};
+
+        for(int i = 0;i < nombres.length; i++){
+            System.out.println(nombres[i]);
+        }
+    }
+}
